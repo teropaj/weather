70940334107eef44baac09b372c23ba2530b0829
teropaj <teropaj@gmail.com>
2018-10-20 11:35:33 +0300

    added React client

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+      response: ''
+    };
+
+    componentDidMount() {
+      this.callApi()
+        .then(res => this.setState({ response: res.express }))
+        .catch(err => console.log(err));
+    }
+
+    callApi = async () => {
+      const response = await fetch('/api/hello');
+      const body = await response.json();
+
+      if (response.status !== 200) throw Error(body.message);
+
+      return body;
+    };
+
+  render() {
+    return (
+      <div  >
+         Hi there!
+      </div>
+    );
+  }
+}
+
+export default App;
